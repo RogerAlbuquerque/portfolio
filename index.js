f9083bdcab44054d0959f8df6b3c5af6fe1d97bb
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2022-12-22 17:38:41 -0300

    algumas mudanças no estilo

---
+++ index.js
@@ -0,0 +1,26 @@
+function showProgramador(){
+  let programador = document.getElementById("programador")
+  let pessoa = document.getElementById("pessoa")
+  
+
+  programador.style.display = "block"
+  pessoa.style.display = "none"
+
+  document.getElementById("comoProgramador").style.color = "#FF5C00"
+  document.getElementById("comoPessoa").style.color = "#00F0FF"
+
+}
+
+function showPessoa(){
+  
+  let programador = document.getElementById("programador")
+  let pessoa = document.getElementById("pessoa")
+
+  programador.style.display = "none"
+  pessoa.style.display = "block"
+
+  document.getElementById("comoProgramador").style.color = "#00F0FF"
+  document.getElementById("comoPessoa").style.color = "#FF5C00"
+
+
+}
